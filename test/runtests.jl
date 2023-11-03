62882e7d2f8d187c7375e7c1a88d4d2bf6f9c274
jeanfdp <jfduple@gmail.com>
2023-11-03 16:51:15 -0400

    Added unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,32 @@ using Test
 @testset "ResistanceDistance.jl" begin
     @testset "resistance_distance_matrix function" begin
         # Create a test graph
-        g = ladder_graph(5)
+        g = ladder_graph(2)
 
         # Calculate the resistance distance matrix
         matrix = resistance_distance_matrix(g)
 
         # Define the expected result
-        expected = ...
+        expected = [0.0   0.7500000000000001  0.7499999999999999  0.9999999999999998
+        0.7500000000000001  0.0   0.9999999999999999   0.7499999999999996
+        0.7499999999999999  0.9999999999999999   0.0   0.7499999999999999
+        0.9999999999999998   0.7499999999999996  0.7499999999999999  0.0]
 
         # Test that the function returns the expected result
         @test matrix == expected
+    end
+
+    @testset "resistance_distance function" begin
+        # Create a test graph
+        g = ladder_graph(2)
+
+        # Calculate the resistance distance between nodes 1 and 2
+        distance = resistance_distance(g,1,2)
+
+        # Define the expected result
+        expected = 0.75
+
+        # Test that the function returns the expected result
+        @test distance == expected
     end
 end
