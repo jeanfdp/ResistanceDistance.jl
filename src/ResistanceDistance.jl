3f9d68bd3370d61a3e85bcef23175f5c546d3bdf
jeanfdp <jfduple@gmail.com>
2023-11-02 20:42:49 -0400

    Bugs fixed

--- src/ResistanceDistance.jl
+++ src/ResistanceDistance.jl
@@ -4,17 +4,18 @@ using Graphs: laplacian_matrix,nv
 using LinearAlgebra: pinv
 
 function resistance_distance_matrix(g)
-    il=pinv(laplacian_matrix(g))
+    il=pinv(Matrix(laplacian_matrix(g)))
     out=zeros(Float64,(nv(g),nv(g)))
     for i in axes(out,1)
-        if j>=i
-            continue
-        end
         for j in axes(out,2)
-            out[i,j]=il[i,i]+il[jj]-2il[i,j]
+            if j>=i
+                break
+            end
+            out[i,j]=il[i,i]+il[j,j]-2il[i,j]
             out[j,i]=out[i,j]
         end
     end
+    return out
 end
 
 end
