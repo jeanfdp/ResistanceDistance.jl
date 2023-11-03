9dd284cec8da61530c24e6d6abc3bba5a0a47466
jeanfdp <jfduple@gmail.com>
2023-11-03 16:33:31 -0400

    Fixed docstrings

--- src/ResistanceDistance.jl
+++ src/ResistanceDistance.jl
@@ -3,23 +3,23 @@ module ResistanceDistance
 using Graphs: laplacian_matrix,nv
 using LinearAlgebra: pinv,det
 
+"""
+    resistance_distance_matrix(g)
+
+Calculate the resistance distance matrix for a graph.
+
+# Arguments
+- `g`: The graph for which the resistance distance matrix is to be calculated.
+
+# Returns
+- A matrix where the element at the i-th row and j-th column represents the resistance distance between nodes `i` and `j`. 
+
+# Notes
+- The resistance distance matrix is calculated using the formula for resistance distance in terms of the Laplacian matrix of the graph.
+- The Laplacian matrix of the graph is calculated using the `laplacian_matrix` function.
+- The pseudoinverse of the Laplacian matrix is calculated using the `pinv` function from the LinearAlgebra module.
+"""
 function resistance_distance_matrix(g)
-    """
-        resistance_distance_matrix(g)
-
-    Calculate the resistance distance matrix for a graph.
-
-    # Arguments
-    - `g`: The graph for which the resistance distance matrix is to be calculated.
-
-    # Returns
-    - A matrix where the element at the i-th row and j-th column represents the resistance distance between nodes `i` and `j`. 
-
-    # Notes
-    - The resistance distance matrix is calculated using the formula for resistance distance in terms of the Laplacian matrix of the graph.
-    - The Laplacian matrix of the graph is calculated using the `laplacian_matrix` function.
-    - The pseudoinverse of the Laplacian matrix is calculated using the `pinv` function from the LinearAlgebra module.
-    """
     il=pinv(Matrix(laplacian_matrix(g)))
     out=zeros(Float64,(nv(g),nv(g)))
     for i in axes(out,1)
@@ -34,24 +34,24 @@ function resistance_distance_matrix(g)
     return out
 end
 
+"""
+    resistance_distance(g, i::Int64, j::Int64)
+
+Calculate the resistance distance between two nodes in a graph.
+
+# Arguments
+- `g`: The graph in which the resistance distance is to be calculated.
+- `i`: The first node.
+- `j`: The second node.
+
+# Returns
+- The resistance distance between nodes `i` and `j`.
+
+# Notes
+- Implements:
+Bapat, Ravindra B., Gutmana, Ivan and Xiao, Wenjun. "A Simple Method for Computing Resistance Distance" Zeitschrift für Naturforschung A, vol. 58, no. 9-10, 2003, pp. 494-498. https://doi.org/10.1515/zna-2003-9-1003
+"""
 function resistance_distance(g,i::Int64,j::Int64)
-    """
-        resistance_distance(g, i::Int64, j::Int64)
-
-    Calculate the resistance distance between two nodes in a graph.
-
-    # Arguments
-    - `g`: The graph in which the resistance distance is to be calculated.
-    - `i`: The first node.
-    - `j`: The second node.
-
-    # Returns
-    - The resistance distance between nodes `i` and `j`.
-
-    # Notes
-    - Implements:
-    Bapat, Ravindra B., Gutmana, Ivan and Xiao, Wenjun. "A Simple Method for Computing Resistance Distance" Zeitschrift für Naturforschung A, vol. 58, no. 9-10, 2003, pp. 494-498. https://doi.org/10.1515/zna-2003-9-1003
-    """
     if i==j
         return 0
     end
