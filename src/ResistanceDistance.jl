c00b6a49f0efd534ba5de9aef825c53f43dd8878
jeanfdp <jfduple@gmail.com>
2023-11-02 20:15:10 -0400

    Added function

--- src/ResistanceDistance.jl
+++ src/ResistanceDistance.jl
@@ -1,5 +1,20 @@
 module ResistanceDistance
 
-# Write your package code here.
+using Graphs: laplacian_matrix,nv
+using LinearAlgebra: pinv
+
+function resistance_distance_matrix(g)
+    il=piv(laplacian_matrix(g))
+    out=zeros(Float64,(nv(g),nv(g)))
+    for i in axes(out,1)
+        if j>=i
+            continue
+        end
+        for j in axes(out,2)
+            out[i,j]=il[i,i]+il[jj]-2il[i,j]
+            out[j,i]=out[i,j]
+        end
+    end
+end
 
 end
